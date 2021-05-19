33462994191b54c32124016af9136dc98f81a77c
Sylvie Canongia <sylvie.canongia.dev@gmail.com>
2021-05-19 10:15:19 +0200

    first commit

---
+++ tasks.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    id: 1,
+    label: 'Coder une todolist en impératif',
+    done: true,
+  },
+  {
+    id: 15,
+    label: 'Coder une todolist en React',
+    done: false,
+  },
+  {
+    id: 3,
+    label: 'Appeler Lucie',
+    done: false,
+  },
+  {
+    id: 7,
+    label: 'Faire un gâteau au chocolat',
+    done: false,
+  },
+  {
+    id: 9,
+    label: 'Promener le chien',
+    done: true,
+  },
+];
